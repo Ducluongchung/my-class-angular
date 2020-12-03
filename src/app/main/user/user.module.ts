0854a357e2f821a7d69501b9b46e533ce98e2371
Ducluongchung <ducluongchung@gmail.com>
2020-12-03 23:52:44 +0700

    add lession 11

--- src/app/main/user/user.module.ts
+++ src/app/main/user/user.module.ts
@@ -1,13 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UserComponent } from './user.component';
-
-
+import { RouterModule, Routes } from '@angular/router';
+export const userRoutes: Routes = [
+  {
+    path: '',
+    redirectTo: 'index',
+    pathMatch: 'full',
+  },
+  {
+    path: 'index',
+    component: UserComponent,
+  },
+];
 
 @NgModule({
   declarations: [UserComponent],
-  imports: [
-    CommonModule
-  ]
+  imports: [CommonModule, RouterModule.forChild(userRoutes)],
 })
-export class UserModule { }
+export class UserModule {}
